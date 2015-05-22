d78acb1cbfd8c98873d476cc92ac9c9e139892a7
Andrei Ismail <iandrei@gmail.com>
2015-05-22 17:11:40 +0300

    #285 Populate db with 4 strings for playing around with them

--- docker/1backend/mysql/schema.sql
+++ docker/1backend/mysql/schema.sql
@@ -3,3 +3,8 @@ CREATE DATABASE test;
 USE test;
 
 CREATE TABLE strings(value LONGTEXT);
+
+INSERT INTO strings(value) VALUES('a');
+INSERT INTO strings(value) VALUES('ab');
+INSERT INTO strings(value) VALUES('abc');
+INSERT INTO strings(value) VALUES('abcd');
