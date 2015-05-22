f4f191c963f37fc0d5c76d8671500b1d2179d0f3
Andrei Ismail <iandrei@gmail.com>
2015-05-22 17:13:15 +0300

    #285 Add base class for ProxySQL tests based on a fleet of containers w/ Docker

--- test/proxysql_base_test.py
+++ test/proxysql_base_test.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import time
 from unittest import TestCase
@@ -6,9 +7,15 @@ from docker import Client
 from docker.utils import kwargs_from_env
 import MySQLdb
 
-class ProxySQLTest(TestCase):
+class ProxySQLBaseTest(TestCase):
 
-	DOCKER_COMPOSE_FILE = "./docker/1backend"
+	DOCKER_COMPOSE_FILE = None
+	PROXYSQL_ADMIN_PORT = 6032
+	PROXYSQL_ADMIN_USERNAME = "admin"
+	PROXYSQL_ADMIN_PASSWORD = "admin"
+	PROXYSQL_RW_PORT = 6033
+	PROXYSQL_RW_USERNAME = "root"
+	PROXYSQL_RW_PASSWORD = "root"
 
 	@classmethod
 	def _startup_docker_services(cls):
@@ -28,7 +35,7 @@ class ProxySQLTest(TestCase):
 	def _get_proxysql_container(cls):
 		containers = Client(**kwargs_from_env()).containers()
 		for container in containers:
-			if 'proxysql' in container['image']:
+			if 'proxysql' in container['Image']:
 				return container
 
 	@classmethod
@@ -55,8 +62,53 @@ class ProxySQLTest(TestCase):
 			subprocess.call(["docker", "exec", container_id, "bash", "/tmp/import_schema.sh"])
 
 	@classmethod
+	def _extract_hostgroup_from_container_name(cls, container_name):
+		service_name = container_name.split('_')[1]
+		return int(re.search(r'BACKEND(\d+)HOSTGROUP(\d+)', service_name).group(2))
+
+	@classmethod
+	def _extract_port_number_from_uri(cls, uri):
+		return int(uri.split(':')[2])
+
+	@classmethod
+	def _get_environment_variables_from_container(cls, container_name):
+		output = Client(**kwargs_from_env()).execute(container_name, 'env')
+		result = {}
+		lines = output.split('\n')
+		for line in lines:
+			line = line.strip()
+			if len(line) == 0:
+				continue
+			(k, v) = line.split('=')
+			result[k] = v
+		return result
+
+	@classmethod
 	def _populate_proxy_configuration_with_backends(cls):
-		pass
+		proxysql_container = cls._get_proxysql_container()
+		mysql_containers = cls._get_mysql_containers()
+		environment_variables = cls._get_environment_variables_from_container(
+											 proxysql_container['Names'][0][1:])
+
+		proxy_admin_connection = MySQLdb.connect("127.0.0.1",
+												cls.PROXYSQL_ADMIN_USERNAME,
+												cls.PROXYSQL_ADMIN_PASSWORD,
+												port=cls.PROXYSQL_ADMIN_PORT)
+		cursor = proxy_admin_connection.cursor()
+
+		for mysql_container in mysql_containers:
+			container_name = mysql_container['Names'][0][1:].upper()
+			port_uri = environment_variables['%s_PORT' % container_name]
+			port_no = cls._extract_port_number_from_uri(port_uri)
+			ip = environment_variables['%s_PORT_%d_TCP_ADDR' % (container_name, port_no)]
+			hostgroup = cls._extract_hostgroup_from_container_name(container_name)
+			cursor.execute("INSERT INTO mysql_servers(hostgroup_id, hostname, port, status) "
+							"VALUES(%d, '%s', %d, 'ONLINE')" %
+							(hostgroup, ip, port_no))
+
+		cursor.execute("LOAD MYSQL SERVERS TO RUNTIME")
+		cursor.close()
+		proxy_admin_connection.close()
 
 	@classmethod
 	def setUpClass(cls):
@@ -73,10 +125,6 @@ class ProxySQLTest(TestCase):
 
 	@classmethod
 	def tearDownClass(cls):
-		pass
+		cls._shutdown_docker_services()
 
-	def test_asd(self):
-		self.assertEqual(1, 1)
-
-	def test_asdf(self):
-		self.assertEqual(1, 1)
+	
