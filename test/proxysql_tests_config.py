b9d8968fdb806ce06dcaab7eacf9bdbf3287084c
Andrei Ismail <iandrei@gmail.com>
2015-08-05 14:18:15 +0300

    #314 Make it possible for config file fields to be overriden in tests

--- test/proxysql_tests_config.py
+++ test/proxysql_tests_config.py
@@ -4,12 +4,16 @@ class ProxySQL_Tests_Config(object):
 
 	DEFAULT_PROXYSQL_TESTS_LOCATION = 'proxysql-tests.ini'
 
-	def __init__(self, location=DEFAULT_PROXYSQL_TESTS_LOCATION):
+	def __init__(self, location=DEFAULT_PROXYSQL_TESTS_LOCATION, overrides={}):
 		self.location = location
 		self.config = ConfigParser()
 		self.config.read(self.location)
+		self.overrides = overrides
 
 	def get(self, section, variable, default=None):
 		""" Returns the value of a variable in a given section, or the default
 		value if the variable or section don't exist."""
+		if section in self.overrides and variable in self.overrides[section]:
+			return self.overrides[section][variable]
+
 		return self.config.get(section, variable)
