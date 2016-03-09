7b7168eadd67ca2eab34bd6ca0ec7248238a7b5f
iprunache <ionut@vitaminsoftware.com>
2016-03-09 19:14:15 +0200

    T99 Implement rate limit for pushing alerts

--- include/AlertRouter.h
+++ include/AlertRouter.h
@@ -1,12 +1,17 @@
 #ifndef PROXYSQL_ALERTROUTER_H
 #define PROXYSQL_ALERTROUTER_H
 
+#include <time.h>
+
 // Acts as a gateway between ProxySQL core and integrations with alerting services.
 // All alerts that need to be pushed to an external service should pass through it.
 // It detects which integrations are enabled and passes the alert to all active
 // integrations.
 class AlertRouter {
+private:
+    time_t lastPushTime;
 public:
+    AlertRouter();
     void pushAlert(const char *message);
 };
 
--- lib/AlertRouter.cpp
+++ lib/AlertRouter.cpp
@@ -7,9 +7,24 @@ extern ProxySQL_Admin *GloAdmin;
 
 // TODO(iprunache) Push alerts in separate threads to prevent blocking the threads that generated the alerts.
 
+
+AlertRouter::AlertRouter() {
+    // Set last push time to the minimum allow interval between pushes in the past to allow alert push immediately.
+    lastPushTime = time(NULL) - GloAdmin->get_min_time_between_alerts_sec() - 1;
+}
+
 // Checks which alert service integrations are enabled and sends the given message as an alert
 // to all enabled services.
 void AlertRouter::pushAlert(const char *message) {
+    // Drop the alert if it is to soon since the last alert was pushed.
+    time_t now = time(NULL);
+    int min_time_between_alerts_sec = GloAdmin->get_min_time_between_alerts_sec();
+    if ((int) difftime(now, lastPushTime) < min_time_between_alerts_sec) {
+        proxy_info("Dropping alert raised sooner than %d seconds since previously pushed alert. Message: %s",
+                   min_time_between_alerts_sec, message);
+        return;
+    }
+
     if (GloAdmin->get_enable_ops_genie_integration()) {
         if (!GloAdmin->get_ops_genie_key() || !GloAdmin->get_ops_genie_recipient()) {
             proxy_error("You need to set both ops_genie_key and ops_genie_recipient to enable integration with OpsGenei");
@@ -19,6 +34,9 @@ void AlertRouter::pushAlert(const char *message) {
         int ret = opsGenieConnector.pushAlert(message);
         if (!ret) {
             proxy_error("Failed to send alert to OpsGenie. Alert message: %s", message);
+        } else {
+            lastPushTime = time(NULL);
         }
     }
 }
+
