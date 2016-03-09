54950efb451e7415c63da73fb2a6b5f8c0c47cae
iprunache <ionut@vitaminsoftware.com>
2016-03-09 18:20:12 +0200

    T99 Switch to mongoose json encoding
    Using mongoose’s emit_json to encode json instead of doing it manually.
    It does string escaping and using it results in more clear and shorter
    code.

--- lib/OpsGenieConnector.cpp
+++ lib/OpsGenieConnector.cpp
@@ -9,15 +9,9 @@ OpsGenieConnector::OpsGenieConnector(const char *apiKey, const char *recipient) 
 }
 
 int OpsGenieConnector::pushAlert(const char *message) {
-    const char *json_format = "{\"message\":\"%s\", \"apiKey\": \"%s\", \"recipients\": [\"%s\"]}";
-    char *json = (char *) malloc(strlen(json_format)
-                                 + strlen(message)
-                                 + strlen(this->apiKey)
-                                 + strlen(this->recipient)
-                                 + 1);
-    sprintf(json, json_format, message, this->apiKey, this->recipient);
+    char json[1000];
+    json_emit(json, 1000, "{s:s, s:s, s:[s]}", "message", message, "apiKey", apiKey, "recipients", recipient);
     http_response *response = http_post(this->apiUrl, "Content-Type: application/json\r\n", json);
-    free(json);
 
     if (response) {
         int r_val = response->status_code == 200;
