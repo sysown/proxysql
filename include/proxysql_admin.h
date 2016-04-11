e4a1066484c217f6f784139af7786921bb4f6175
René Cannaò <rene.cannao@gmail.com>
2016-04-11 22:54:36 +0000

    Upgrade from mysql_query_rules
    mysql_query_rules table is changing from version to version.
    In order to provide online upgrade, a new function was added:
    - ProxySQL_Admin::disk_upgrade_mysql_query_rules()

--- include/proxysql_admin.h
+++ include/proxysql_admin.h
@@ -74,6 +74,7 @@ class ProxySQL_Admin {
 	bool set_variable(char *name, char *value);
 	void flush_admin_variables___database_to_runtime(SQLite3DB *db, bool replace);
 	void flush_admin_variables___runtime_to_database(SQLite3DB *db, bool replace, bool del, bool onlyifempty);
+	void disk_upgrade_mysql_query_rules();
 
 #ifdef DEBUG
 	void add_credentials(char *type, char *credentials, int hostgroup_id);
