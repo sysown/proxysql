749e2845141f22c2a23fb8698707d2753a1bc712
René Cannaò <rene.cannao@gmail.com>
2016-06-12 15:51:13 +0000

    More verbose output in proxysql_error() and proxysql_warning()

--- include/proxysql_debug.h
+++ include/proxysql_debug.h
@@ -27,7 +27,9 @@ extern int gdbg;
 #define proxy_debug(module, verbosity, fmt, ...)
 #endif /* DEBUG */
 
+/*
 #ifdef DEBUG
+*/
 #define proxy_error(fmt, ...) \
 	do { \
 		time_t __timer; \
@@ -38,6 +40,7 @@ extern int gdbg;
 		strftime(__buffer, 25, "%Y-%m-%d %H:%M:%S", __tm_info); \
 		proxy_error_func("%s %s:%d:%s(): [ERROR] " fmt, __buffer, __FILE__, __LINE__, __func__ , ## __VA_ARGS__); \
 	} while(0)
+/*
 #else
 #define proxy_error(fmt, ...) \
 	do { \
@@ -50,8 +53,10 @@ extern int gdbg;
     proxy_error_func("%s [ERROR] " fmt , __buffer , ## __VA_ARGS__); \
 	} while(0)
 #endif
-
+*/
+/*
 #ifdef DEBUG
+*/
 #define proxy_warning(fmt, ...) \
 	do { \
 		time_t __timer; \
@@ -62,6 +67,7 @@ extern int gdbg;
 		strftime(__buffer, 25, "%Y-%m-%d %H:%M:%S", __tm_info); \
 		proxy_error_func("%s %s:%d:%s(): [WARNING] " fmt, __buffer, __FILE__, __LINE__, __func__ , ## __VA_ARGS__); \
 	} while(0)
+/*
 #else
 #define proxy_warning(fmt, ...) \
 	do { \
@@ -74,7 +80,7 @@ extern int gdbg;
     proxy_error_func("%s [WARNING] " fmt , __buffer , ## __VA_ARGS__); \
 	} while(0)
 #endif
-
+*/
 #ifdef DEBUG
 #define proxy_info(fmt, ...) \
 	do { \
