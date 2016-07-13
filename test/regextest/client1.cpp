1561ffff8720da88a7d99a1fa496c3040d68498f
René Cannaò <rene.cannao@gmail.com>
2016-07-13 16:55:44 +0000

    Testing rewrite

--- test/regextest/client1.cpp
+++ test/regextest/client1.cpp
@@ -21,8 +21,13 @@ int main() {
 	char *myq=(char *)"sEt   NAmEs    'utf8' ";
 	r->re=new RE2(" *SET  *NAMES *.* *", *r->opt);
 	bool rc;
-	for (int i=0;i<1000000;i++) {
+	for (int i=0;i<100000;i++) {
+		string *new_query=new std::string(myq);
 		rc=RE2::PartialMatch(myq,*r->re);
+		//RE2::Replace(new_query,(char *)" *(\\w+)  *(.*) *(.*) *",(char *)"\1 \2 \3 a");
+		RE2::Replace(new_query,(char *)" *(\\w+)\\s+(\\w+)\\s+(\\w+)\\s*",(char *)"\\1 \\2 \\3");
+		//std::cout << new_query->c_str() << std::endl;
+		delete new_query;
 	}
 	printf("%d\n",rc);
 	return 0;
