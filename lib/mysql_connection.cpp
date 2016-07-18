db00c9a9047a6969bbe1f97a63247bf10cb8c35e
René Cannaò <rene.cannao@gmail.com>
2016-07-18 15:47:07 +0000

    Bug fix #609
    INIT_DB wasn't working properly if the new schemaname was a prefix of the old schemaname

--- lib/mysql_connection.cpp
+++ lib/mysql_connection.cpp
@@ -119,7 +119,11 @@ void MySQL_Connection_userinfo::set(MySQL_Connection_userinfo *ui) {
 
 
 bool MySQL_Connection_userinfo::set_schemaname(char *_new, int l) {
-	if ((schemaname==NULL) || (strncmp(_new,schemaname,l))) {
+	int _l=0;
+	if (schemaname) {
+		_l=strlen(schemaname); // bug fix for #609
+	}
+	if ((schemaname==NULL) || (strncmp(_new,schemaname, (l > _l ? l : _l) ))) {
 		if (schemaname) {
 			free(schemaname);
 			schemaname=NULL;
