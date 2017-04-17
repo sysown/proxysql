9206e718dd4d601ded7a3c544708fcf7fa427fa0
René Cannaò <rene.cannao@gmail.com>
2017-04-17 07:18:09 +0000

    Second possible fix for bug #896

--- lib/MySQL_Thread.cpp
+++ lib/MySQL_Thread.cpp
@@ -2367,7 +2367,7 @@ __run_skip_1a:
 							mysess->thread=NULL;
 							// we first delete the association in sessmap
 							sessmap.erase(mysess->thread_session_id);
-							if (mysql_sessions->len) {
+							if (mysql_sessions->len > 1) {
 								// take the last element and adjust the map
 								MySQL_Session *mysess_last=(MySQL_Session *)mysql_sessions->index(mysql_sessions->len-1);
 								sessmap[mysess_last->thread_session_id]=sess_pos;
@@ -2462,7 +2462,7 @@ __run_skip_1a:
 						mysess->thread=NULL;
 						// we first delete the association in sessmap
 						sessmap.erase(mysess->thread_session_id);
-						if (mysql_sessions->len) {
+						if (mysql_sessions->len > 1) {
 						// take the last element and adjust the map
 						MySQL_Session *mysess_last=(MySQL_Session *)mysql_sessions->index(mysql_sessions->len-1);
 							sessmap[mysess_last->thread_session_id]=sess_pos;
