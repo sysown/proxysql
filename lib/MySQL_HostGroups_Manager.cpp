4d431e402af5e581a3ee82116b48724199a949f0
René Cannaò <rene.cannao@gmail.com>
2018-03-20 04:12:14 +0100

    Bug fix for memory leak in LOAD MYSQL SERVERS TO RUNTIME #1425

--- lib/MySQL_HostGroups_Manager.cpp
+++ lib/MySQL_HostGroups_Manager.cpp
@@ -1109,6 +1109,7 @@ void MySQL_HostGroups_Manager::generate_mysql_group_replication_hostgroups_table
 		}
 	}
 	sqlite3_finalize(statement);
+	delete incoming_group_replication_hostgroups;
 	incoming_group_replication_hostgroups=NULL;
 
 	// remove missing ones
@@ -1686,6 +1687,10 @@ void MySQL_HostGroups_Manager::set_incoming_replication_hostgroups(SQLite3_resul
 }
 
 void MySQL_HostGroups_Manager::set_incoming_group_replication_hostgroups(SQLite3_result *s) {
+	if (incoming_group_replication_hostgroups) {
+		delete incoming_group_replication_hostgroups;
+		incoming_group_replication_hostgroups = NULL;
+	}
 	incoming_group_replication_hostgroups=s;
 }
 
