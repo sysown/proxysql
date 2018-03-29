b11bdef8075cbb8bb62d26a110aedcefb179f1bf
René Cannaò <rene.cannao@gmail.com>
2018-03-29 17:22:11 +0200

    HTTP: moving funtions into private

--- include/ProxySQL_HTTP_Server.hpp
+++ include/ProxySQL_HTTP_Server.hpp
@@ -11,6 +11,11 @@ class ProxySQL_HTTP_Server {
 	time_t cur_time;
 	pthread_mutex_t check_version_mutex;
 	time_t last_check_version;
+	std::string * generate_header(char *);
+	std::string * generate_canvas(char *);
+	std::string * generate_chart(char *chart_name, char *ts, int nsets, char **dname, char **llabel, char **values);
+	char *extract_values(SQLite3_result *result, int idx, bool relative, double mult=1);
+	char *extract_ts(SQLite3_result *result, bool relative);
 	public:
 	struct {
 		char *proxysql_latest_version;
@@ -20,11 +25,6 @@ class ProxySQL_HTTP_Server {
 	void init();
 	int handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **ptr);
 	void print_version();
-	std::string * generate_header(char *);
-	std::string * generate_canvas(char *);
-	std::string * generate_chart(char *chart_name, char *ts, int nsets, char **dname, char **llabel, char **values);
-	char *extract_values(SQLite3_result *result, int idx, bool relative, double mult=1);
-	char *extract_ts(SQLite3_result *result, bool relative);
 	void check_latest_version_http();
 };
 
