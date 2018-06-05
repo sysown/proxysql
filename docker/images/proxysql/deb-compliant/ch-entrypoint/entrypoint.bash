d3798957f2298aedbda99066f23e1bdd209b25f1
Nick Vyzas <pondix@gmail.com>
2018-06-05 14:03:40 +0300

    Adding Ubuntu Bionic and Fedora 28 - Includes new package building process with docker-compose

---
+++ docker/images/proxysql/deb-compliant/ch-entrypoint/entrypoint.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Delete package if exists
+rm -f /opt/proxysql/binaries/proxysql_${CURVER}-${PKG_RELEASE}_amd64.deb || true &&
+# Cleanup relic directories from a previously failed build
+rm -f /opt/proxysql/proxysql.ctl || true &&
+# Clean and build dependancies and source
+cd /opt/proxysql && \
+${MAKE} cleanbuild && \
+${MAKE} ${MAKEOPT} build_deps_clickhouse && \
+${MAKE} clickhouse ${MAKEOPT} && \
+# Prepare package files and build RPM
+cp /root/ctl/proxysql.ctl /opt/proxysql/proxysql.ctl && \
+cp /opt/proxysql/src/proxysql /opt/proxysql/ && \
+equivs-build proxysql.ctl && \
+mv /opt/proxysql/proxysql_${CURVER}_amd64.deb ./binaries/proxysql_${CURVER}-${PKG_RELEASE}_amd64.deb && \
+# Cleanup current build
+rm -f /opt/proxysql/proxysql.ctl /opt/proxysql/proxysql
