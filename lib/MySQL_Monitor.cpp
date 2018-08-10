sysown/proxysql

commit 88fdec89a828589c1b3c93f4d112fcda945cb290

Use SELECT @@global.read_only #1621

lib/MySQL_Monitor.cpp
@@ -664,12 +664,12 @@ void * monitor_read_only_thread(void *arg) {
 	mmsd->t1=monotonic_time();
 	mmsd->interr=0; // reset the value
 	if (mmsd->task_id == MON_INNODB_READ_ONLY) {
-		mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SHOW GLOBAL VARIABLES LIKE 'innodb_read_only'");
+		mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SELECT @@global.innodb_read_only");
 	} else {
 		if (mmsd->task_id == MON_SUPER_READ_ONLY) {
-			mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SHOW GLOBAL VARIABLES LIKE 'super_read_only'");
+			mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SELECT @@global.super_read_only");
 		} else {
-			mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SHOW GLOBAL VARIABLES LIKE 'read_only'");
+			mmsd->async_exit_status=mysql_query_start(&mmsd->interr,mmsd->mysql,"SELECT @@global.read_only");
 		}
 	}
 	while (mmsd->async_exit_status) {
@@ -745,7 +745,7 @@ __exit_monitor_read_only_thread:
 			fields = mysql_fetch_fields(mmsd->result);
 			for(k = 0; k < num_fields; k++) {
 				//if (strcmp("VARIABLE_NAME", fields[k].name)==0) {
-				if (strcmp((char *)"Value", (char *)fields[k].name)==0) {
+				if (strcmp((char *)"@@global.read_only", (char *)fields[k].name)==0) {
 					j=k;
 				}
 			}

commit 7158e601d14bfe9b9c573e312988d0b088077e45

Unable to parse innodb_read_only variable #1641
This is a recession bug introduced in 88fdec89a828589c1b3c93f4d112fcda945cb290

lib/MySQL_Monitor.cpp
@@ -1958,10 +1958,10 @@ void * MySQL_Monitor::monitor_read_only() {
 					MySQL_Monitor_State_Data *mmsd=new MySQL_Monitor_State_Data(r->fields[0],atoi(r->fields[1]), NULL, atoi(r->fields[2]));
 					mmsd->task_id = MON_READ_ONLY; // default
 					if (r->fields[3]) {
-						if (strcasecmp(r->fields[3],(char *)"innodb_read_only")==0) {
+						if (strcasecmp(r->fields[3],(char *)"@@global.innodb_read_only")==0) {
 							mmsd->task_id = MON_INNODB_READ_ONLY;
 						} else {
-							if (strcasecmp(r->fields[3],(char *)"super_read_only")==0) {
+							if (strcasecmp(r->fields[3],(char *)"@@global.super_read_only")==0) {
 								mmsd->task_id = MON_SUPER_READ_ONLY;
 							}
 						}