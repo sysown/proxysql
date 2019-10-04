49ab0b59279d0d11227eb2f8cecc9c2befbcda9f
Valentin Rakush <valentin@sysown.com>
2019-10-04 19:50:53 +0000

    stmt closing

--- lib/mysql_connection.cpp
+++ lib/mysql_connection.cpp
@@ -2174,8 +2174,7 @@ int MySQL_Connection::async_send_simple_command(short event, char *stmt, unsigne
 
 void MySQL_Connection::query_close_stmt() {
 	if (query.stmt) {
-		if (mysql_stmt_close(query.stmt))
-			proxy_error("Error closing stmt");
+		mysql_stmt_free(query.stmt);
 		query.stmt = NULL;
 	}
 }
