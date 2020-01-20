417cb90c14d9bb036745ca2ee830a8c495e62933
René Cannaò <rene.cannao@gmail.com>
2020-01-20 23:47:33 +1100

    Generalize the use of session variables
    Added new array mysql_tracked_variables[] that defines the tracked variables.
    Ideally, tracking a new session variable will requires adding only a couple of lines of code.
    
    Removed session variables from mysql_thread_variables_names:
    - now both MySQL_Threads_Handler::get_variables_list() and MySQL_Threads_Handler::has_variable() rely on mysql_tracked_variables[]
    
    Simplified MySQL_Variables, as it now also uses mysql_tracked_variables[] .

--- include/MySQL_Thread.h
+++ include/MySQL_Thread.h
@@ -17,16 +17,16 @@
 #define SQLITE_HOSTGROUP -4
 
 
-#define MYSQL_DEFAULT_SQL_MODE	""
-#define MYSQL_DEFAULT_TIME_ZONE	"SYSTEM"
-#define MYSQL_DEFAULT_ISOLATION_LEVEL	"READ COMMITTED"
-#define MYSQL_DEFAULT_TRANSACTION_READ	"WRITE"
+//#define MYSQL_DEFAULT_SQL_MODE	""
+//#define MYSQL_DEFAULT_TIME_ZONE	"SYSTEM"
+//#define MYSQL_DEFAULT_ISOLATION_LEVEL	"READ COMMITTED"
+//#define MYSQL_DEFAULT_TRANSACTION_READ	"WRITE"
 #define MYSQL_DEFAULT_TX_ISOLATION	"READ-COMMITTED"
-#define MYSQL_DEFAULT_CHARACTER_SET_RESULTS	"NULL"
-#define MYSQL_DEFAULT_SESSION_TRACK_GTIDS	"OFF"
-#define MYSQL_DEFAULT_SQL_AUTO_IS_NULL	"OFF"
-#define MYSQL_DEFAULT_SQL_SELECT_LIMIT	"DEFAULT"
-#define MYSQL_DEFAULT_SQL_SAFE_UPDATES	"OFF"
+//#define MYSQL_DEFAULT_CHARACTER_SET_RESULTS	"NULL"
+//#define MYSQL_DEFAULT_SESSION_TRACK_GTIDS	"OFF"
+//#define MYSQL_DEFAULT_SQL_AUTO_IS_NULL	"OFF"
+//#define MYSQL_DEFAULT_SQL_SELECT_LIMIT	"DEFAULT"
+//#define MYSQL_DEFAULT_SQL_SAFE_UPDATES	"OFF"
 #define MYSQL_DEFAULT_COLLATION_CONNECTION	""
 #define MYSQL_DEFAULT_NET_WRITE_TIMEOUT	"60"
 #define MYSQL_DEFAULT_MAX_JOIN_SIZE	"18446744073709551615"
@@ -365,19 +365,20 @@ class MySQL_Threads_Handler
 		char *init_connect;
 		char *ldap_user_variable;
 		char *add_ldap_user_comment;
-		char *default_sql_mode;
-		char *default_time_zone;
-		char *default_isolation_level;
-		char *default_transaction_read;
+		//char *default_sql_mode;
+		//char *default_time_zone;
+//		char *default_isolation_level;
+//		char *default_transaction_read;
 		char *default_tx_isolation;
-		char *default_character_set_results;
-		char *default_session_track_gtids;
-		char *default_sql_auto_is_null;
-		char *default_sql_select_limit;
-		char *default_sql_safe_updates;
+//		char *default_character_set_results;
+//		char *default_session_track_gtids;
+//		char *default_sql_auto_is_null;
+		//char *default_sql_select_limit;
+		//char *default_sql_safe_updates;
 		char *default_collation_connection;
 		char *default_net_write_timeout;
 		char *default_max_join_size;
+		char *default_variables[SQL_NAME_LAST];
 		char *firewall_whitelist_errormsg;
 #ifdef DEBUG
 		bool session_debug;
