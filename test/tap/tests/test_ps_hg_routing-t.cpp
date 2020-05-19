6a6d157f2f9eb9e933a24d43abf9c293d8bb8417
Valentin Rakush <valentin@sysown.com>
2020-05-19 06:23:58 +0000

    removed dependency on replication

--- test/tap/tests/test_ps_hg_routing-t.cpp
+++ test/tap/tests/test_ps_hg_routing-t.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
 	if(cl.getEnv())
 		return exit_status();
 
-	plan(10);
+	plan(1);
 	diag("Testing PS host groups routing");
 
 	MYSQL* mysqladmin = mysql_init(NULL);
@@ -60,25 +60,6 @@ int main(int argc, char** argv) {
 	MYSQL_QUERY(mysqladmin, "insert into mysql_query_rules (rule_id, active, flagIN, match_digest, negate_match_pattern, re_modifiers, destination_hostgroup, apply) values (200, 1, 0, '^SELECT', 0, 'CASELESS', 1, 1)");
 	MYSQL_QUERY(mysqladmin, "load mysql query rules to runtime");
 
-	const int MAX_NUM_REPS = 60;
-	int count = 0;
-	for (;;) {
-		diag("Waiting for test.test1 [%d] seconds", count);
-		int rc = mysql_query(mysql, "select * from test.test1");
-		if (rc == 0) {
-			MYSQL_RES *r1 = mysql_store_result(mysql);
-			if (r1)
-				mysql_free_result(r1);
-			break;
-		}
-		sleep(1);
-		count++;
-		if (count >= MAX_NUM_REPS) {
-			ok(false, "Cannot execute query 'select * from test.test1'. Test failed.");
-			break;
-		}
-	}
-
 	MYSQL_QUERY(mysql, "set autocommit=0");
 	MYSQL_STMT *stmt = mysql_stmt_init(mysql);
 	if (!stmt)
@@ -89,20 +70,13 @@ int main(int argc, char** argv) {
 
 	std::string query = "SELECT * FROM test.test1";
 	if (mysql_stmt_prepare(stmt, query.c_str(), query.size())) {
-		fprintf(stderr, "Query error %s\n", mysql_error(mysql));
+		ok(false, "Query error %s\n", mysql_error(mysql));
 		mysql_close(mysql);
 		mysql_library_end();
 		return exit_status();
 	}
-	ok(true, "Statement prepared");
 
-	if (mysql_stmt_execute(stmt))
-	{
-		fprintf(stderr, " mysql_stmt_execute(), failed\n");
-		ok(false, " %s\n", mysql_stmt_error(stmt));
-		return exit_status();
-	}
-	ok(true, "Statement executed");
+	MYSQL_QUERY(mysql, "update test.test1 set c1='aaaaaa'");
 
 	MYSQL_BIND bind[2];
 	char char_data0[STRING_SIZE];
@@ -126,76 +100,30 @@ int main(int argc, char** argv) {
 	bind[1].length= &length[1];
 	bind[1].error= &error[1];
 
-	if (mysql_stmt_bind_result(stmt, bind))
-	{
-		fprintf(stderr, " mysql_stmt_bind_result() failed\n");
-		ok(false, " %s\n", mysql_stmt_error(stmt));
-		return exit_status();
-	}
-
-	if (mysql_stmt_store_result(stmt))
-	{
-		fprintf(stderr, " mysql_stmt_store_result() failed\n");
-		ok(false, " %s\n", mysql_stmt_error(stmt));
-		return exit_status();
-	}
-	ok(true, "Result stored");
-
-	while (!mysql_stmt_fetch(stmt))	{
-		ok(strcmp((char*)bind[0].buffer, "abcdef") == 0 &&  strcmp((char*)bind[1].buffer, "abcdef") == 0, "Data received");
-	}
-	ok(true, "Result fetched");
-
-	MYSQL_QUERY(mysql, "update test.test1 set c1='aaaaaa'");
-
-	ok(true, "Record is updated");
-
 	if (mysql_stmt_execute(stmt))
 	{
-		fprintf(stderr, " mysql_stmt_execute(), failed\n");
 		ok(false, " %s\n", mysql_stmt_error(stmt));
 		return exit_status();
 	}
 
-	ok(true, "Statement executed second time");
-
-	bind[0].buffer_type= MYSQL_TYPE_STRING;
-	bind[0].buffer= (char *)&char_data0;
-	bind[0].buffer_length= STRING_SIZE;
-	bind[0].is_null= &is_null[0];
-	bind[0].length= &length[0];
-	bind[0].error= &error[0];
-
-	bind[1].buffer_type= MYSQL_TYPE_STRING;
-	bind[1].buffer= (char *)&char_data1;
-	bind[1].buffer_length= STRING_SIZE;
-	bind[1].is_null= &is_null[1];
-	bind[1].length= &length[1];
-	bind[1].error= &error[1];
-
 	if (mysql_stmt_bind_result(stmt, bind))
 	{
-		fprintf(stderr, " mysql_stmt_bind_result() failed\n");
 		ok(false, " %s\n", mysql_stmt_error(stmt));
 		return exit_status();
 	}
 
 	if (mysql_stmt_store_result(stmt))
 	{
-		fprintf(stderr, " mysql_stmt_store_result() failed\n");
 		ok(false, " %s\n", mysql_stmt_error(stmt));
 		return exit_status();
 	}
-	ok(true, "Result stored");
 
 	while (!mysql_stmt_fetch(stmt))	{
 		ok(strcmp((char*)bind[0].buffer, "aaaaaa") == 0, "Read value that was updated. Expected [aaaaaa]. Actual [%s]", bind[0].buffer);
 	}
-	ok(true, "Result fetched");
 
 	if (mysql_stmt_close(stmt))
 	{
-		fprintf(stderr, " failed while closing the statement\n");
 		ok(false, " %s\n", mysql_error(mysql));
 		return exit_status();
 	}
