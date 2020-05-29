c1354f884d16c32fe65b22960edfc1f9b7eccd4e
René Cannaò <rene.cannao@gmail.com>
2020-05-29 22:18:02 +0200

    set_testing do not connect to admin at all

--- test/tap/tests/set_testing-t.cpp
+++ test/tap/tests/set_testing-t.cpp
@@ -552,7 +552,6 @@ int main(int argc, char *argv[]) {
 	MYSQL_QUERY(mysqladmin, "update global_variables set variable_value='true' where variable_name='mysql-enforce_autocommit_on_reads'");
 	MYSQL_QUERY(mysqladmin, "load mysql variables to runtime");
 
-	mysql_close(mysqladmin);
 */
 	MYSQL* mysql = mysql_init(NULL);
 	if (!mysql)
@@ -621,9 +620,5 @@ int main(int argc, char *argv[]) {
 	for (unsigned int i=0; i<num_threads; i++) {
 		pthread_join(thi[i], NULL);
 	}
-	MYSQL_QUERY(mysqladmin, "update global_variables set variable_value='false' where variable_name='mysql-enforce_autocommit_on_reads'");
-	MYSQL_QUERY(mysqladmin, "load mysql variables to runtime");
-
-	mysql_close(mysqladmin);
 	return exit_status();
 }
