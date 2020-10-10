8bce4982cc31a93cf594d11400909e29ba657b5b
Richard Pounder <richard.pounder@creditkarma.com>
2020-10-10 12:09:38 -0700

    fix removal of wrong session in session_list #2460

--- lib/MySQL_Session.cpp
+++ lib/MySQL_Session.cpp
@@ -6388,9 +6388,11 @@ void MySQL_Session::create_new_session_and_reset_connection(MySQL_Data_Stream *_
 	if (new_myds->mypolls==NULL) {
 		thread->mypolls.add(POLLIN|POLLOUT, new_myds->fd, new_myds, thread->curtime);
 	}
-	int rc = new_sess->handler();
+
+	// store the idx now in case handler mutates the list of sessions to stop bugs like #2460
+    unsigned int sess_idx = thread->mysql_sessions->len-1;
+    int rc = new_sess->handler();
 	if (rc==-1) {
-		unsigned int sess_idx = thread->mysql_sessions->len-1;
 		thread->unregister_session(sess_idx);
 		delete new_sess;
 	}
