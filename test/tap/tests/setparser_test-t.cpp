0d89a6edf0dce8e8b969dfd121e3b2c6c1761860
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2021-02-24 13:00:54 +0000

    Added wrapper tap test for previous 'setparser' toguether with new test cases for it

---
+++ test/tap/tests/setparser_test-t.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file setparser_test-t.cpp
+ * @brief This test is a simple wrapper for calling 'setparser_test' as a tap test.
+ */
+
+#include "command_line.h"
+#include "tap.h"
+
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	CommandLine cl;
+
+	if(cl.getEnv())
+		return exit_status();
+
+	plan(1);
+	std::string test_bin { std::string { cl.workdir } + "setparser_test" };
+	int setparser_err = system(test_bin.c_str());
+
+	ok (setparser_err == 0, "Executing 'setparser_test' returned err code: %d", setparser_err);
+	return exit_status();
+}
+
