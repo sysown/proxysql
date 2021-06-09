64ae971ff59b704127d8f7cb227525b9e38f33c1
Christos Soulios <1561376+csoulios@users.noreply.github.com>
2021-06-10 01:29:07 +0300

    Removed methods that were not used

--- include/MySQL_LDAP_Authentication.hpp
+++ include/MySQL_LDAP_Authentication.hpp
@@ -5,8 +5,8 @@ class MySQL_LDAP_Authentication {
 public:
 	MySQL_LDAP_Authentication() {};
 	virtual ~MySQL_LDAP_Authentication() {};
-	virtual bool add(char *username, char *backend_username, char *password, enum cred_username_type usertype, bool use_ssl, int default_hostgroup, char *default_schema, bool schema_locked, bool transaction_persistent, bool fast_forward, int max_connections) {return false;};
-	virtual bool del(char *username, enum cred_username_type usertype, bool set_lock=true) {return false;};
+	//virtual bool add(char *username, char *backend_username, char *password, enum cred_username_type usertype, bool use_ssl, int default_hostgroup, char *default_schema, bool schema_locked, bool transaction_persistent, bool fast_forward, int max_connections) {return false;};
+	// virtual bool del(char *username, enum cred_username_type usertype, bool set_lock=true) {return false;};
 	virtual bool reset() {return false;};
 	virtual void print_version() {};
 	virtual char * lookup(char *username, char *pass, 
@@ -17,7 +17,7 @@ public:
 	virtual int increase_frontend_user_connections(char *username, int *mc=NULL) {return 0;};
 	virtual void decrease_frontend_user_connections(char *username) {};
 	virtual void set_all_inactive(enum cred_username_type usertype) {};
-	virtual void remove_inactives(enum cred_username_type usertype) {};
+	// virtual void remove_inactives(enum cred_username_type usertype) {};
 	virtual bool set_SHA1(char *username, enum cred_username_type usertype, void *sha_pass) {return false;};
 	virtual char **get_variables_list() {return NULL;}
 	virtual bool has_variable(const char *name) {return false;};
