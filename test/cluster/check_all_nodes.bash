74b05ca0d2e8f3d1f169535b3a66b7c9074a0375
René Cannaò <rene.cannao@gmail.com>
2021-08-26 13:48:59 +0200

    Reduce the number of mysql instances in check_all_nodes.bash

--- test/cluster/check_all_nodes.bash
+++ test/cluster/check_all_nodes.bash
@@ -8,13 +8,12 @@ for i in ${!TABLES[@]} ; do
 	ALL_TABLES+=("runtime_"${TABLES[$i]})
 done
 
-for i in ${!ALL_TABLES[@]} ; do
-	echo "SELECT * FROM ${ALL_TABLES[$i]};"
-done
+#for i in ${!ALL_TABLES[@]} ; do
+#	echo "SELECT * FROM ${ALL_TABLES[$i]};"
+#done
 
-
-for i in ${!ALL_TABLES[@]} ; do
-	for p in 6032 `seq 26001 26009` ; do 
-		mysql -u admin -padmin -h 127.0.0.1 -P$p -e "SELECT COUNT(*) FROM ${ALL_TABLES[$i]}" > /dev/null 2> /dev/null &
-	done
+for p in 6032 `seq 26001 26009` ; do
+	for i in ${!ALL_TABLES[@]} ; do
+		echo "SELECT COUNT(*) FROM ${ALL_TABLES[$i]};"
+	done | mysql -u admin -padmin -h 127.0.0.1 -P$p > /dev/null 2> /dev/null &
 done
