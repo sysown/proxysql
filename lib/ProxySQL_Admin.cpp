0058ecb4f07e9ec6ad4ba6249b798b90a890e66f
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2021-09-16 19:17:02 +0200

    Removed 'flush_error_log' call from 'PROXYSQL FLUSH QUERY CACHE'

--- lib/ProxySQL_Admin.cpp
+++ lib/ProxySQL_Admin.cpp
@@ -1528,7 +1528,6 @@ bool admin_handler_command_proxysql(char *query_no_space, unsigned int query_no_
 		if (GloQC) {
 			GloQC->flush();
 		}
-		SPA->flush_error_log();
 		SPA->send_MySQL_OK(&sess->client_myds->myprot, NULL);
 		return false;
 	}
