330feaae8b74121fe0a6222f3f875cf1f55f7ff9
René Cannaò <rene.cannao@gmail.com>
2022-01-05 12:18:24 +0100

    Changed variables order in PtrSizeArray/PtrArray
    This minor change seems to improve cache affinity, because "len" is the
    variable that is read mostly

--- include/gen_utils.h
+++ include/gen_utils.h
@@ -41,9 +41,9 @@ class PtrArray {
 		size=new_size;
 	}
 	public:
-	void **pdata;
 	unsigned int len;
 	unsigned int size;
+	void **pdata;
 	PtrArray(unsigned int __size=0) {
 		len=0;
 		pdata=NULL;
@@ -128,9 +128,9 @@ class PtrSizeArray {
 	public:
 	void * operator new(size_t);
 	void operator delete(void *);
-	PtrSize_t *pdata;
 	unsigned int len;
 	unsigned int size;
+	PtrSize_t *pdata;
 	PtrSizeArray(unsigned int __size=0);
 	~PtrSizeArray();
 
