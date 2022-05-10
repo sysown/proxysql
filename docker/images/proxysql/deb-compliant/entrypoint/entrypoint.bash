6fc9c4508702ff502dc2c5999feb4428074433e6
Miro Stauder <miro@nixnux.net>
2022-05-10 19:17:18 +0000

    fix reproducible builds

--- docker/images/proxysql/deb-compliant/entrypoint/entrypoint.bash
+++ docker/images/proxysql/deb-compliant/entrypoint/entrypoint.bash
@@ -24,7 +24,11 @@ rm -f /opt/proxysql/proxysql.ctl /opt/proxysql/proxysql || true
 echo "==> Building"
 git config --global --add safe.directory '/opt/proxysql'
 cd /opt/proxysql
+echo "==> ProxySQL '$(git describe --long --abbrev=7)'"
 export SOURCE_DATE_EPOCH=$(git show -s --format=%ct HEAD)
+echo "==> Setting SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}"
+find /opt/proxysql -exec touch --date=@${SOURCE_DATE_EPOCH} {} \;
+
 if [[ -z ${PROXYSQL_BUILD_TYPE:-} ]] ; then
 	deps_target="build_deps"
 	build_target=""
--- docker/images/proxysql/suse-compliant/entrypoint/entrypoint.bash
+++ docker/images/proxysql/suse-compliant/entrypoint/entrypoint.bash
@@ -24,7 +24,11 @@ rm -fr /root/.pki /root/rpmbuild/{BUILDROOT,RPMS,SRPMS,BUILD,SOURCES,tmp} /opt/p
 echo "==> Building"
 git config --global --add safe.directory '/opt/proxysql'
 cd /opt/proxysql
+echo "==> ProxySQL '$(git describe --long --abbrev=7)'"
 export SOURCE_DATE_EPOCH=$(git show -s --format=%ct HEAD)
+echo "==> Setting SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}"
+find /opt/proxysql -exec touch --date=@${SOURCE_DATE_EPOCH} {} \;
+
 if [[ -z ${PROXYSQL_BUILD_TYPE:-} ]] ; then
 	deps_target="build_deps"
 	build_target=""
