7e80539c3edaf352233e7e9bb700f1d524ebf648
René Cannaò <rene.cannao@gmail.com>
2022-12-21 05:50:26 +0000

    More bugfixes and testing on SSL and fast_forward
    In a previous commit we fixed the fact that if more than 16KB of data was
    sent to Admin, SQLite3 Server or Clickhouse Server, no further data was
    detected thus blocking.
    It seems that in some circumstances data can be returned in chunks of 4KB,
    thus we removed the 16KB logic replacing it with multiples of 4KB.

--- lib/ProxySQL_Admin.cpp
+++ lib/ProxySQL_Admin.cpp
@@ -5466,9 +5466,11 @@ void *child_mysql(void *arg) {
 			// PMC-10004
 			// we probably should use SSL_pending() and/or SSL_has_pending() to determine
 			// if there is more data to be read, but it doesn't seem to be working.
-			// Therefore we hardcored the value 16384 (16KB) as a special case and
-			// we try to call read_from_net() again
-			while (rb == 16384) {
+			// Therefore we hardcored the values 4096 (4K) as a special case and
+			// we try to call read_from_net() again.
+			// Previously we hardcoded 16KB but it seems that it can return in smaller
+			// chunks of 4KB.
+			while (rb > 0 && rb%4096 == 0) {
 				rb = myds->read_from_net();
 				if (myds->net_failure) goto __exit_child_mysql;
 				myds->read_pkts();
