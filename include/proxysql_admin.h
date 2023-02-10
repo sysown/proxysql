748aa596d606062dd98c383fd7eba015edd2cde4
Javier Sánchez Parra <javsanpar@riseup.net>
2023-02-10 10:52:44 +0100

    Create stats_mysql_query_digest statements directly from the digest_umap

--- include/proxysql_admin.h
+++ include/proxysql_admin.h
@@ -5,6 +5,7 @@
 #include <prometheus/counter.h>
 #include <prometheus/gauge.h>
 
+#include "query_processor.h"
 #include "proxy_defines.h"
 #include "proxysql.h"
 #include "cpp.h"
@@ -417,6 +418,10 @@ class ProxySQL_Admin {
 
 	void p_update_metrics();
 	void stats___mysql_query_rules();
+	void stats___save_mysql_query_digest_to_sqlite(
+		const bool reset, const bool copy, const SQLite3_result *resultset,
+		const umap_query_digest *digest_umap, const umap_query_digest_text *digest_text_umap
+	);
 	void stats___mysql_query_digests(bool reset, bool copy=false);
 	//void stats___mysql_query_digests_reset();
 	void stats___mysql_commands_counters();
