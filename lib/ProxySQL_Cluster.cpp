5bdb8e11e517c1f068fd7feb02ccb2e8bb25b1b5
Rahim Kanji <rahim.kanji@outlook.com>
2024-02-15 13:22:08 +0500

    Fixed error message

--- lib/ProxySQL_Cluster.cpp
+++ lib/ProxySQL_Cluster.cpp
@@ -4454,7 +4454,7 @@ void ProxySQL_Node_Address::resolve_hostname() {
 		const std::string& ip = MySQL_Monitor::dns_lookup(hostname, false, &ip_count);
 
 		if (ip_count > 1) {
-			proxy_warning("ProxySQL Cluster node '%s' has more than one (%ld) mapped IP address: under some circumstances this may lead to undefined behavior. It is recommended to provide IP address or hostname with only one resolvable IP.\n",
+			proxy_error("Proxy cluster node '%s' has more than one ('%ld') mapped IP address. It is recommended to provide IP address or domain with one resolvable IP address.\n",
 				hostname, ip_count);
 		}
 
