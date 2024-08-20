dbc51ab1130315d1bb13970f7e437333bc5c0293
Miro Stauder <miro@proxysql.com>
2024-08-20 07:36:10 +0000

    remove unused files

--- test/tap/tests_grp_auto_increment_delay_multiplex/pre-tests_grp_auto_increment_delay_multiplex-proxysql.sql
+++
@@ -1,4 +0,0 @@
-# proxysql settings
-SET mysql-auto_increment_delay_multiplex=0;
-LOAD MYSQL VARIABLES TO RUNTIME;
-SAVE MYSQL VARIABLES TO DISK;
--- test/tap/tests_grp_query_digests_keep_comment/pre-tests_grp_query_digests_keep_comment-proxysql.sql
+++
@@ -1,4 +0,0 @@
-# proxysql settings
-SET mysql-query_digests_keep_comment=1;
-LOAD MYSQL VARIABLES TO RUNTIME;
-SAVE MYSQL VARIABLES TO DISK;
