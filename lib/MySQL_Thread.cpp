97eb19c0f0e92ba33a6d1086e5b36e0e9dde4713
Wazir Ahmed <wazirahmedf@gmail.com>
2025-08-01 10:47:55 +0530

    fix: Close idle session based on effective timeout
    - effective_timeout = min (mysql-wait_timeout, session-wait_timeout)
    
    Signed-off-by: Wazir Ahmed <wazir@proxysql.com>

--- lib/MySQL_Thread.cpp
+++ lib/MySQL_Thread.cpp
@@ -3808,7 +3808,7 @@ void MySQL_Thread::ProcessAllSessions_MaintenanceLoop(MySQL_Session *sess, unsig
 			}
 		} else {
 			// the session is idle, kill it
-			if (sess_time/1000 > (unsigned long long)sess->wait_timeout) {
+			if (sess_time/1000 > effective_wait_timeout) {
 				sess->killed=true;
 				if (sess->client_myds) {
 					proxy_warning("Killing client connection %s:%d because inactive for %llums\n",sess->client_myds->addr.addr,sess->client_myds->addr.port, sess_time/1000);
