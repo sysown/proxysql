494dbf37fc103347ca6faf6603d1eb7b447e5389
Rene Cannao <rene.cannao@gmail.com>
2025-11-19 02:51:20 +0000

    Minor enhancement in TAP fast_forward_grace_close
    Added `CREATE DATABASE IF NOT EXISTS test`

--- test/tap/tests/fast_forward_grace_close.cpp
+++ test/tap/tests/fast_forward_grace_close.cpp
@@ -67,6 +67,7 @@ int main() {
 		return -1;
 	}
 	ok(1, "Connected to backend server");
+	MYSQL_QUERY(backend_conn, "CREATE DATABASE IF NOT EXISTS test");
 	MYSQL_QUERY(backend_conn, "CREATE TABLE IF NOT EXISTS dummy_log_table (id INT PRIMARY KEY AUTO_INCREMENT, data LONGTEXT)");
 	MYSQL_QUERY(backend_conn, "INSERT INTO dummy_log_table (data) VALUES (REPEAT('a', 1024*50))");
 	MYSQL_QUERY(backend_conn, "INSERT INTO dummy_log_table (data) VALUES (REPEAT('a', 1024*50))");
