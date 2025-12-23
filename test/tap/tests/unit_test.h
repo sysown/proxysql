67ca39e248c60861c5d7194d6f114714f7ea8a65
Wazir Ahmed <wazirahmedf@gmail.com>
2025-12-24 02:23:27 +0530

    doc: Update TAP documentation with unit test instructions
    Signed-off-by: Wazir Ahmed <wazir@proxysql.com>

---
+++ test/tap/tests/unit_test.h
@@ -0,0 +1,17 @@
+/**
+ * The following macro and headers are required to avoid various linker errors when compiling unit tests.
+ * TODO: Fix this by improving include hierarchy.
+ */
+
+#define PROXYSQL_EXTERN
+#define EXCLUDE_TRACKING_VARAIABLES
+#define MAIN_PROXY_SQLITE3
+
+#include <memory>
+#include <cstdint>
+#include <cstring>
+#include "openssl/ssl.h"
+#include "sqlite3.h"
+#include "proxysql_structs.h"
+#include "MySQL_LDAP_Authentication.hpp"
+MySQL_LDAP_Authentication* GloMyLdapAuth = nullptr;
