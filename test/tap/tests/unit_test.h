40a1c787f79ba8910cef841b28a5ce53171baa87
Wazir Ahmed <wazirahmedf@gmail.com>
2025-12-25 13:47:27 +0530

    TAP: Add proper comments in unit_test.h
    Signed-off-by: Wazir Ahmed <wazir@proxysql.com>

--- test/tap/tests/unit_test.h
+++ test/tap/tests/unit_test.h
@@ -1,6 +1,9 @@
+#ifndef UNIT_TEST_H
+#define UNIT_TEST_H
+
 /**
- * The following macro and headers are required to avoid various linker errors when compiling unit tests.
- * TODO: Fix this by improving include hierarchy.
+ * The following macros and headers are required to avoid various linker errors when compiling unit tests.
+ * TODO: Fix this by improving the include hierarchy.
  */
 
 #define PROXYSQL_EXTERN
@@ -14,4 +17,19 @@
 #include "sqlite3.h"
 #include "proxysql_structs.h"
 #include "MySQL_LDAP_Authentication.hpp"
+
+/**
+ * IMPORTANT: This is an ODR (One Definition Rule) violation.
+ * DO NOT copy this pattern in production code or other contexts.
+ *
+ * Background: GloMyLdapAuth is declared as 'extern' in multiple source files in the 'lib' directory.
+ * Since unit tests link against ProxySQL libraries without the full binary, this causes linker errors.
+ *
+ * This definition provides a stub implementation to satisfy the linker when building unit tests.
+ * This is acceptable because:
+ *   1. Unit tests are standalone executables that don't share symbols with each other
+ *   2. This is a temporary workaround until the include hierarchy is properly refactored
+ */
 MySQL_LDAP_Authentication* GloMyLdapAuth = nullptr;
+
+#endif // UNIT_TEST_H
