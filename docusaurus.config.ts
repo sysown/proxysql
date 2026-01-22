4d4ab096d15ccb78578f4089bcdbbab4448419c9
René Cannaò <rene.cannao@gmail.com>
2026-01-22 17:49:43 +0100

    Fix baseUrl for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://sysown.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/proxysql/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
