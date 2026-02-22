3a2513c73bafbb9c3622d818f8889bbf2a3e74bb
Rene Cannao <rene@proxysql.com>
2026-02-22 13:18:00 +0000

    test: inject MySQL v2 and Prometheus noise into PostgreSQL tests
    Integrated the following noise routines into 5 key PostgreSQL TAP tests:
    - internal_noise_mysql_traffic_v2 (100 conns, 300ms delay)
    - internal_noise_prometheus_poller
    - internal_noise_rest_prometheus_poller (auto-enabled)
    
    Updated the following tests:
    - pgsql-basic_tests-t
    - pgsql-query_cache_test-t
    - pgsql-reg_test_5300_threshold_resultset_deadlock-t
    - pgsql-set_statement_test-t
    - pgsql-extended_query_protocol_test-t
    
    Ensured correct 'noise_utils.h' inclusion and dynamic TAP plan adjustments.

--- test/tap/tests/pgsql-basic_tests-t.cpp
+++ test/tap/tests/pgsql-basic_tests-t.cpp
@@ -648,7 +648,7 @@ int main(int argc, char** argv) {
     if (cl.getEnv())
         return exit_status();
 
-    spawn_internal_noise(cl, internal_noise_mysql_traffic);
+    spawn_internal_noise(cl, internal_noise_mysql_traffic_v2, {{"num_connections", "100"}, {"reconnect_interval", "100"}, {"avg_delay_ms", "300"}});
     spawn_internal_noise(cl, internal_noise_prometheus_poller);
     spawn_internal_noise(cl, internal_noise_pgsql_traffic);
     spawn_internal_noise(cl, internal_noise_rest_prometheus_poller, {{"enable_rest_api", "true"}});
