3ac0c2dd8a2c918f4f3fc636371c912cd38dfc10
Rene Cannao <rene@proxysql.com>
2026-03-21 10:20:00 +0000

    Fix legacy TAP tests: clickhouse_php_conn and reg_test_3317
    - clickhouse_php_conn-t.php: Fix deprecated ClickHouse MergeTree syntax
      Change from deprecated ENGINE=MergeTree(EventDate, (EventDate), 8192)
      to new syntax ENGINE=MergeTree() ORDER BY (EventDate)
    
    - reg_test_3317-lock_hostgroup_special_queries-t.cpp: Add query annotations
      Add /* ;hostgroup=1300 */ hints to SELECT statements to ensure proper
      routing when connection is locked to hostgroup 1300

--- test/tap/tests/reg_test_3317-lock_hostgroup_special_queries-t.cpp
+++ test/tap/tests/reg_test_3317-lock_hostgroup_special_queries-t.cpp
@@ -34,7 +34,7 @@ void check_set_names(MYSQL* proxysql_mysql) {
 		return;
 	}
 
-	query_res = mysql_query(proxysql_mysql, "SELECT @@character_set_client, @@character_set_results, @@character_set_connection");
+	query_res = mysql_query(proxysql_mysql, "SELECT /* ;hostgroup=1300 */ @@character_set_client, @@character_set_results, @@character_set_connection");
 	if (query_res) {
 		diag("Query failed with error: %s", mysql_error(proxysql_mysql));
 		return;
@@ -78,7 +78,7 @@ void check_set_names(MYSQL* proxysql_mysql) {
  * @param proxysql_mysql A MYSQL handle to an already stablished MySQL connection.
  */
 void check_autocommit(MYSQL* proxysql_mysql) {
-	int query_res = mysql_query(proxysql_mysql, "SELECT @@autocommit");
+	int query_res = mysql_query(proxysql_mysql, "SELECT /* ;hostgroup=1300 */ @@autocommit");
 	if (query_res) {
 		diag("Query failed with error: %s", mysql_error(proxysql_mysql));
 		return;
@@ -105,7 +105,7 @@ void check_autocommit(MYSQL* proxysql_mysql) {
 	}
 
 	// Check new status on @@autocommit
-	query_res = mysql_query(proxysql_mysql, "SELECT @@autocommit");
+	query_res = mysql_query(proxysql_mysql, "SELECT /* ;hostgroup=1300 */ @@autocommit");
 	if (query_res) {
 		diag("Query failed with error: %s", mysql_error(proxysql_mysql));
 		return;
@@ -140,7 +140,7 @@ void check_session_character_set_server(MYSQL* proxysql_mysql) {
 		return;
 	}
 
-	query_res = mysql_query(proxysql_mysql, "SELECT @@character_set_server");
+	query_res = mysql_query(proxysql_mysql, "SELECT /* ;hostgroup=1300 */ @@character_set_server");
 	if (query_res) {
 		diag("Query failed with error: %s", mysql_error(proxysql_mysql));
 		return;
@@ -179,7 +179,7 @@ void check_session_character_set_results(MYSQL* proxysql_mysql) {
 		return;
 	}
 
-	query_res = mysql_query(proxysql_mysql, "SELECT @@character_set_results");
+	query_res = mysql_query(proxysql_mysql, "SELECT /* ;hostgroup=1300 */ @@character_set_results");
 	if (query_res) {
 		diag("Query failed with error: %s", mysql_error(proxysql_mysql));
 		return;
