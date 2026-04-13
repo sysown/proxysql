a3da04a4dab10d1687cdb0c77b3d7ebf44577301
Rene Cannao <rene@proxysql.com>
2026-04-13 12:37:54 +0000

    fix(infra): add local_infile=ON to MySQL 8.4 dbdeployer entrypoints
    MySQL 8.4 defaults local_infile to OFF. The old infra-mysql84 my.cnf
    had local_infile=ON. Without this, test_unsupported_queries-t fails
    because LOAD DATA LOCAL INFILE is disabled on the backend.

--- test/infra/infra-dbdeployer-mysql84-binlog/docker/entrypoint.sh
+++ test/infra/infra-dbdeployer-mysql84-binlog/docker/entrypoint.sh
@@ -27,6 +27,7 @@ dbdeployer deploy replication "${MYSQL_VERSION}" \
     -c binlog_format=ROW \
     -c max_connections=500 \
     -c max_connect_errors=100000 \
+    -c local_infile=ON \
     -c innodb_buffer_pool_size=128M \
     -c innodb_redo_log_capacity=64M \
     -c innodb_flush_log_at_trx_commit=2 \
