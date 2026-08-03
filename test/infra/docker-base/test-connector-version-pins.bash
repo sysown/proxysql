7aa0881a3dff1590c157b508811b7308fbf7b3b1
Rene Cannao <rene@proxysql.com>
2026-08-03 10:39:22 +0000

    test: validate quoted connector dependencies

--- test/infra/docker-base/test-connector-version-pins.bash
+++ test/infra/docker-base/test-connector-version-pins.bash
@@ -11,7 +11,8 @@ fi
 
 for package in mysql-connector-python mysqlx-connector-python; do
     expected_pin="${package}==\${MYSQL_CONNECTOR_PYTHON_VERSION}"
-    if ! rg -Fq -- "$expected_pin" "$dockerfile_path"; then
+    expected_dependency="\"${expected_pin}\" \\"
+    if ! rg -Fq -- "$expected_dependency" "$dockerfile_path"; then
         echo "ERROR: missing package pin: $expected_pin" >&2
         exit 1
     fi
