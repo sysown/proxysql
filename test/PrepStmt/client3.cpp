6401c8cc2c827956205cbcd94dcddb35574d1d5e
Rene Cannao <rene@proxysql.com>
2026-08-10 15:58:35 +0000

    PrepStmt: use snprintf for client3 query strings
    Replace unsafe sprintf calls in client3 benchmark with bounded snprintf for deterministic SQL string construction.

--- test/PrepStmt/client3.cpp
+++ test/PrepStmt/client3.cpp
@@ -98,7 +98,7 @@ void * mysql_thread(int tid) {
 			fprintf(stderr, " mysql_stmt_init(), out of memory\n");
 			exit(EXIT_FAILURE);
 		}
-		sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+		snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 		bl=strlen(buff);
 		uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 		MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
@@ -120,7 +120,7 @@ void * mysql_thread(int tid) {
 		unsigned int founds=0;
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+			snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			//uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 			//MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
@@ -132,7 +132,7 @@ void * mysql_thread(int tid) {
 		unsigned int founds=0;
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+		snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 			//MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
@@ -144,7 +144,7 @@ void * mysql_thread(int tid) {
 		unsigned int founds=0;
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+		snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 			MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
@@ -156,7 +156,7 @@ void * mysql_thread(int tid) {
 		unsigned int founds=0;
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+		snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 			MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
@@ -176,7 +176,7 @@ void * mysql_thread(int tid) {
 		// for comparison, we run also queries in TEXT protocol
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + %u",i,(uint32_t)mt_rand()%NUMPRO);
+		snprintf(buff,sizeof(buff),"SELECT %u + %u",i,(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			int rc=mysql_real_query(mysql,buff,bl);
 			if (rc) {
