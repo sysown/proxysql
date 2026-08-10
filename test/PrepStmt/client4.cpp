8f9382fecaa4dd8d2720e0ba18d250014af0276f
Rene Cannao <rene@proxysql.com>
2026-08-10 15:59:36 +0000

    PrepStmt: finish remaining sprintf replacement in client4
    Replace final active sprintf use in client4 prepared-statement loop with bounded snprintf.

--- test/PrepStmt/client4.cpp
+++ test/PrepStmt/client4.cpp
@@ -148,7 +148,7 @@ void * mysql_thread() {
 		unsigned int founds=0;
 		cpu_timer t;
 		for (i=0; i<NUMPREP*LOOPS; i++) {
-			sprintf(buff,"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
+			snprintf(buff,sizeof(buff),"SELECT %u + ?",(uint32_t)mt_rand()%NUMPRO);
 			bl=strlen(buff);
 			uint64_t hash=local_stmts->compute_hash(0,(char *)USER,(char *)SCHEMA,buff,bl);
 			MySQL_STMT_Global_info *a=GloMyStmt->find_prepared_statement_by_hash(hash);
