100770723334827ec98ee6fa36dbeada52a7c393
Rene Cannao <rene@proxysql.com>
2026-08-10 15:48:48 +0000

    aurora: avoid repeated strlen in quoted table parsing

--- test/tap/tests/aurora.cpp
+++ test/tap/tests/aurora.cpp
@@ -372,10 +372,12 @@ void SQLite3_Server_session_handler(MySQL_Session *sess, void *_pa, PtrSize_t *p
 			tbh=dbh;
 			dbh=strdup("main");
 		}
-		if (strlen(tbh)>=3 && tbh[0]=='`' && tbh[strlen(tbh)-1]=='`') { // tablename is quoted
-			char *tbh_tmp=(char *)malloc(strlen(tbh)-1);
-			strncpy(tbh_tmp,tbh+1,strlen(tbh)-2);
-			tbh_tmp[strlen(tbh)-2]=0;
+		size_t tbh_len = strlen(tbh);
+		if (tbh_len>=3 && tbh[0]=='`' && tbh[tbh_len-1]=='`') { // tablename is quoted
+			size_t db_len = tbh_len - 2;
+			char *tbh_tmp=(char *)malloc(db_len+1);
+			memcpy(tbh_tmp,tbh+1,db_len);
+			tbh_tmp[db_len]=0;
 			free(tbh);
 			tbh=tbh_tmp;
 		}
