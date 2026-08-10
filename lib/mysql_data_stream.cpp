a9a589ef12dd2420f754f1bf6171a2ec1f453441
Rene Cannao <rene@proxysql.com>
2026-08-10 09:01:40 +0000

    Guard passthrough cleartext scrub length before memset

--- lib/mysql_data_stream.cpp
+++ lib/mysql_data_stream.cpp
@@ -401,8 +401,11 @@ MySQL_Data_Stream::~MySQL_Data_Stream() {
 		if (passthrough_cleartext) {
 			// Best-effort scrub before free; the cleartext password should
 			// not linger in freed heap memory.
-			const size_t cleartext_len = strlen(passthrough_cleartext);
-			memset(passthrough_cleartext, 0, cleartext_len);
+			const char* safe_cleartext = passthrough_cleartext ? passthrough_cleartext : "";
+			const size_t cleartext_len = strlen(safe_cleartext);
+			if (cleartext_len) {
+				memset(passthrough_cleartext, 0, cleartext_len);
+			}
 			free(passthrough_cleartext);
 			passthrough_cleartext = NULL;
 		}
